4e5fedb8b0bf7b8bc0f0259f3a38dd794bef2ba9
KoyaMiyahara <141590159+mhky0331@users.noreply.github.com>
2023-10-07 14:24:23 +0000

    コントローラー作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
-
+  namespace :user do
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    root to: 'homes#top'
+  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
 
+  scope module: :user do
+    root to: 'homes#top'
+  end
   # 顧客用
   # URL /users/sign_in ...
   devise_for :users, skip: [:passwords],  controllers: {
   registrations: "user/registrations",
   sessions: 'user/sessions'
   }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
