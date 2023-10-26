36340a2b31cfc03a9d8bfa562866e6e62d12dc00
KoyaMiyahara <141590159+mhky0331@users.noreply.github.com>
2023-10-26 13:05:22 +0000

    fixed migrate

--- db/migrate/20231008083352_create_facilities.rb
+++ db/migrate/20231008083352_create_facilities.rb
@@ -3,7 +3,7 @@ class CreateFacilities < ActiveRecord::Migration[6.1]
     create_table :facilities, comment: '施設' do |t|
       t.integer :user_id, null: false
       t.string :name, comment: '施設名', null: false, default: ""
-      t.text :content, comment: '投稿内容', null: false, default: ""
+      t.text :content, comment: '投稿内容', null: false
       t.float :lat, comment: '緯度', null: false, default: 0
       t.float :lng, comment: '経度', null: false, default: 0
       t.timestamps
