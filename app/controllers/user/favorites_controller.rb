b346ca605dab21235959f7d706fce61f38a3aafd
KoyaMiyahara <141590159+mhky0331@users.noreply.github.com>
2023-11-20 12:14:05 +0000

    お気に入り修正

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,6 +1,7 @@
 class User::FavoritesController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :set_facility, only:[:create,:destroy]
 
   def index
     favorites = Favorite.where(user_id: params[:user_id]).pluck(:facility_id)
@@ -15,5 +16,8 @@ class User::FavoritesController < ApplicationController
     @favorite = Favorite.find_by(user_id: current_user.id, facility_id: @facility.id)
     @favorite.destroy
   end
-
+  private
+  def set_facility
+    @facility = Facility.find(params[:facility_id])
+  end
 end
