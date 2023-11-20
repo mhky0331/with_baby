a551d3fd8f80cf2fc0be82c9670f9548fe70e738
KoyaMiyahara <141590159+mhky0331@users.noreply.github.com>
2023-11-20 14:42:38 +0000

    seed書きたい

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 管理者パスワード
 Admin.create!(
    name: "wb",
    email:"admin@admin",
 password:  "111111"
   )
+
+  # ユーザー
+[
+  ['test1@test.com', 'はる', profile_image: File.open('./app/assets/images/user_no_image.jpg'), password: 111111],
+  ['test2@test.com', 'なつ', profile_image: File.open('./app/assets/images/user_no_image.jpg'), password: 111111],
+  ['test3@test.com', 'あき', profile_image: File.open('./app/assets/images/user_no_image.jpg'), password: 111111],
+  ['test4@test.com', 'ふゆ', profile_image: File.open('./app/assets/images/user_no_image.jpg'), password: 111111]
+].each do |mail, name, profile_image, password|
+  User.create!(
+    { email: mail, name: name, profile_image: profile_image, password: password}
+  )
+  end
+
